541d92ae3e924b452f43ac7d1ba3873f7514a7a5
Jaehwan Park <turnaround0@gmail.com>
2019-10-11 10:25:20 +0900

    Update consistency.py

--- features/consistency.py
+++ features/consistency.py
@@ -1,7 +1,7 @@
 # Consistency features 1: ans_stddev
-# Consistency features 2: que_stddev
 def getStdevOfScoresOfAnswers(posts):
     return
 
+# Consistency features 2: que_stddev
 def getStdevOfScoresOfQuestions(posts):
     return
