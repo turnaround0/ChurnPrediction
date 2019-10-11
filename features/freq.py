098606ec0f93edd6fed678bb5269c7be04684831
Jaehwan Park <turnaround0@gmail.com>
2019-10-11 11:52:04 +0900

    Update freq.py

--- features/freq.py
+++ features/freq.py
@@ -1,18 +1,20 @@
 # Frequency features 1: num_answers
 def getNumAnswers(posts_group):
-    return posts_group['AnswerCount'].sum()
+    return posts_group.Answer.sum()
 
 
 # Frequency features 2: num_questions
 def getNumQuestions(posts_group):
-    return posts_group.size()
+    return posts_group.Question.sum()
 
 
 # Frequency features 3: ans_ques_ratio
 def getAnsQuesRatio(num_answers, num_questions):
-    return num_answers / num_questions
+    df = num_answers / num_questions
+    df[num_questions == 0] = 0
+    return df
 
 
 # Frequency features 4: num_posts
 def getNumPosts(posts_group):
-    return posts_group.size() + posts_group['AnswerCount'].sum()
+    return posts_group.size()
