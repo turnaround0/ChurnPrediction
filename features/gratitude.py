2d78ddc35243b0a043d1e3102e54163dc28a7680
Jaehwan Park <turnaround0@gmail.com>
2019-10-11 10:26:08 +0900

    Update gratitude.py

--- features/gratitude.py
+++ features/gratitude.py
@@ -1,8 +1,8 @@
 # Gratitude features 1: ans_comments
-# Gratitude features 2: que_comments
 def getAvgNumOfAnswers(posts):
     return
 
+# Gratitude features 2: que_comments
 def getAvgNumOfQuestions(posts):
     return
 
