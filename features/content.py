77b39aa949bfc39ecc983807f885146423c6510a
Jaehwan Park <turnaround0@gmail.com>
2019-10-11 10:25:40 +0900

    Update content.py

--- features/content.py
+++ features/content.py
@@ -1,7 +1,7 @@
 # Content features 1: ans_length
-# Content features 2: que_length
 def getLengthOfAnswers(posts):
     return
 
+# Content features 2: que_length
 def getLengthOfQuestions(posts):
     return
