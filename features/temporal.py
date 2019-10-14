a7fd7d88f65f5ecb74b76e653ce308af5fec333b
Jaehwan Park <turnaround0@gmail.com>
2019-10-14 17:48:05 +0900

    Update temporal.py

--- features/temporal.py
+++ features/temporal.py
@@ -2,36 +2,45 @@ import pandas as pd
 
 
 # Temporal features 1: gap1
-def getTimeGap1OfUser(posts_group):
-    posts_group_1 = posts_group.nth(1)
-    gap1 = posts_group_1.CreationDate - posts_group_1.CreationDateOfOwner
-    return gap1
+def getTimeGap1OfUser(users, posts):
+    # CreationDateOfOwner of users is merged to posts dataframe when preprocessing data
+    posts_group_1 = posts.groupby('OwnerUserId').nth(1)
+    gap_1 = (posts_group_1.CreationDate - posts_group_1.CreationDateOfOwner).dropna() / pd.Timedelta('1 minute')
+    gap_1.index.name = 'Id'
+    return gap_1
 
 
 # Temporal features 2: gapK
-def getTimeGapsOfPosts(posts_group, K):
+def getTimeGapsOfPosts(posts, K):
+    posts_group = posts.groupby('OwnerUserId')
+    posts_group_k_prior = posts_group.nth(K - 1)
     posts_group_k = posts_group.nth(K)
-    return posts_group_k.gap
-    # posts_group_k_minus_1 = posts_group.nth(K - 1)
-    # posts_group_k = posts_group.nth(K)
-    # gap_k = (posts_group_k.CreationDate - posts_group_k_minus_1.CreationDate).dropna() / pd.Timedelta('1 hour')
-    # return gap_k
+    gap_k = (posts_group_k.CreationDate - posts_group_k_prior.CreationDate).dropna() / pd.Timedelta('1 minute')
+    gap_k.index.name = 'Id'
+    return gap_k
 
 
 # Temporal features 3: last_gap
-def getTimeLastGapOfPosts(posts_group):
-    return getTimeGapsOfPosts(posts_group, -1)
+def getTimeLastGapOfPosts(posts):
+    gap_last = getTimeGapsOfPosts(posts, -1)
+    gap_last.index.name = 'Id'
+    return gap_last
 
 
 # Temporal features 4: time_since_last_post
-def getTimeSinceLastPost(posts_group, end_date):
-    posts_group_last = posts_group.nth(-1)
+def getTimeSinceLastPost(users, posts):
+    end_date = pd.to_datetime('2012-07-31')
+    posts_group_last = posts.groupby('OwnerUserId').nth(-1)
     gap_since_last = pd.to_datetime(end_date) - posts_group_last.CreationDate
+    gap_since_last.index.name = 'Id'
     return gap_since_last
 
 
 # Temporal features 5: mean_gap
 def getTimeMeanGap(posts):
-    gap_posts = posts[['gap', 'OwnerUserId']]
-    gap_posts = gap_posts[gap_posts.gap != 0]
-    return gap_posts.groupby('OwnerUserId').mean()
+    posts_group = posts.groupby('OwnerUserId')
+    assert()
+    # gap_posts = posts[['gap', 'OwnerUserId']]
+    # gap_posts = gap_posts[gap_posts.gap != 0]
+    # return gap_posts.groupby('OwnerUserId').mean()
+    return posts
