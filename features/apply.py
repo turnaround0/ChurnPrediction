f68b079ab1816f1335ae6ee931c7bd8e5b253041
Jaehwan Park <turnaround0@gmail.com>
2019-10-15 16:50:34 +0900

    Update apply.py

--- features/apply.py
+++ features/apply.py
@@ -1,4 +1,4 @@
-from features import temporal, tasks, freq
+from features import temporal, tasks, freq, know
 
 
 def getFeatures(features, users, posts, task, K=None, T=None):
@@ -93,3 +93,37 @@ def apply_frequency_features_of_task2(features_of_task2, users_of_task2, posts_o
         features_of_task2[T]['ans_que_ratio'] = \
             freq.getAnsQuesRatio(features_of_task2[T]['num_answers'], features_of_task2[T]['num_questions'])
         features_of_task2[T]['num_posts'] = freq.getNumPosts(posts)
+
+
+def apply_knowledge_features_of_task1(features_of_task1, users_of_task1, posts_of_task1, posts_df):
+    # Extract knowledge features of task 1
+    list_of_K = range(1, 21)
+    for K in list_of_K:
+        print("Extract knowledge features of task1(K=", K, ")")
+        users, posts = users_of_task1[K], posts_of_task1[K]
+        answers, questions, qnta, tqna = know.preprocessForKnowledgeFeaturesForTask1(users, posts, posts_df)
+        features_of_task1[K]['accepted_answerer_rep'] = know.getRepOfAcceptedAnswerer(users, answers, questions, qnta, tqna)
+        features_of_task1[K]['max_rep_answerer'] = know.getMaxRepAmongAnswerer(users, answers, questions, qnta, tqna)
+        features_of_task1[K]['num_que_answered'] = know.getNumQueAnswered(users, answers, questions, qnta, tqna)
+        features_of_task1[K]['time_for_first_ans'] = know.getTimeForFirstAns(users, answers, questions, qnta, tqna)
+        features_of_task1[K]['rep_questioner'] = know.getAvgRepOfQuestioner(users, answers, questions, qnta, tqna)
+        features_of_task1[K]['rep_answerers'] = know.getAvgRepOfAnswerer(users, answers, questions, qnta, tqna)
+        features_of_task1[K]['rep_co_answerers'] = know.getAvgRepOfCoAnswerer(users, answers, questions, qnta, tqna)
+        features_of_task1[K]['num_answers_recvd'] = know.getAvgNumAnsReceived(users, answers, questions, qnta, tqna)
+        print(features_of_task1[K].accepted_answerer_rep)
+
+
+def apply_knowledge_features_of_task2(features_of_task2, users_of_task2, posts_of_task2):
+    list_of_T = [7, 15, 30]
+    for T in list_of_T:
+        print("Extract knowledge features of task2(T=)", T, ")")
+        users, posts = users_of_task2[T], posts_of_task2[T]
+        answers, questions, qna, qna1 = know.preprocessForKnowledgeFeaturesForTask2(users, posts)
+        features_of_task2[T]['accepted_answerer_rep'] = know.getRepOfAcceptedAnswerer(users, answers, questions, qna, qna1)
+        features_of_task2[T]['max_rep_answerer'] = know.getMaxRepAmongAnswerer(users, answers, questions, qna, qna1)
+        features_of_task2[T]['num_que_answered'] = know.getNumQueAnswered(users, answers, questions, qna, qna1)
+        features_of_task2[T]['time_for_first_ans'] = know.getTimeForFirstAns(users, answers, questions, qna, qna1)
+        features_of_task2[T]['rep_questioner'] = know.getAvgRepOfQuestioner(users, answers, questions, qna, qna1)
+        features_of_task2[T]['rep_answerers'] = know.getAvgRepOfAnswerer(users, answers, questions, qna, qna1)
+        features_of_task2[T]['rep_co_answerers'] = know.getAvgRepOfCoAnswerer(users, answers, questions, qna, qna1)
+        features_of_task2[T]['num_answers_recvd'] = know.getAvgNumAnsReceived(users, answers, questions, qna, qna1)
