168ee52cc9ec4e550d6046a4f8d209b233447a7c
Jaehwan Park <turnaround0@gmail.com>
2019-10-17 17:09:01 +0900

    Update train_models.py

--- train/train_models.py
+++ train/train_models.py
@@ -1,8 +1,24 @@
 from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC, LinearSVC
 
 
-def LogisticRegressionWithArgs(*arg, **kwarg):
-    kwarg['max_iter'] = 1e3
-    # kwarg['solver'] = 'lbfgs'
-    kwarg['tol'] = 1e2
-    return LogisticRegression(*arg, **kwarg)
+def logistic_regression_with_args(*arg, **kwargs):
+    kwargs['max_iter'] = 1e3
+    # kwargs['solver'] = 'saga'
+    kwargs['tol'] = 1e2
+    kwargs['random_state'] = 1234
+    return LogisticRegression(*arg, **kwargs)
+
+
+def svc_with_args(*args, **kwargs):
+    kwargs['max_iter'] = 1e3
+    kwargs['tol'] = 1e2
+    kwargs['random_state'] = 1234
+    return SVC(*args, **kwargs)
+
+
+def linear_svc_with_args(*args, **kwargs):
+    kwargs['max_iter'] = 1e3
+    kwargs['tol'] = 1e2
+    kwargs['random_state'] = 1234
+    return LinearSVC(*args, **kwargs)
