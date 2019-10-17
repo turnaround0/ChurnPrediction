0014ef0aaa14b9da2a77a35e59de6f811b59dfc0
Jaehwan Park <turnaround0@gmail.com>
2019-10-17 17:11:35 +0900

    Update analysis_train.py

--- analysis/analysis_train.py
+++ analysis/analysis_train.py
@@ -71,7 +71,7 @@ def plot_figure5_of_task2(list_of_T, task2_accuracy_of_category, is_display=Fals
 
 
 def plot_multi_figure5_of_task1(list_of_K, task1_accuracy_of_category, is_display=False):
-    fig, axs = plt.subplots(2, 5, figsize=(6.4 * 5, 4.8 * 2))
+    fig, axs = plt.subplots(2, 5, figsize=(6.4 * 4, 4.8 * 2))
     ax_list = [ax for sub_axs in axs for ax in sub_axs]
 
     for idx, (title, predictions) in enumerate(task1_accuracy_of_category.items()):
@@ -96,7 +96,7 @@ def plot_multi_figure5_of_task1(list_of_K, task1_accuracy_of_category, is_displa
 
 
 def plot_multi_figure5_of_task2(list_of_T, task2_accuracy_of_category, is_display=False):
-    fig, axs = plt.subplots(2, 5, figsize=(6.4 * 5, 4.8 * 2))
+    fig, axs = plt.subplots(2, 5, figsize=(6.4 * 4, 4.8 * 2))
     ax_list = [ax for sub_axs in axs for ax in sub_axs]
 
     for idx, (title, predictions) in enumerate(task2_accuracy_of_category.items()):
