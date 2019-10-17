d2e7ca649a4384accdd76e5dc4b5df7d13ad477f
Jaehwan Park <turnaround0@gmail.com>
2019-10-17 17:10:54 +0900

    Update analysis_train.py

--- analysis/analysis_train.py
+++ analysis/analysis_train.py
@@ -45,7 +45,7 @@ def plot_figure5_of_task1(list_of_K, task1_accuracy_of_category, is_display=Fals
         ax.bar(index, predictions, tick_label=list_of_K, align='center')
         ax.set_title(title)
         ax.set_xlim(-1, n_groups)
-        ax.set_ylim(40, 100)
+        ax.set_ylim(50, 80)
         fig.savefig('output/figure5_task1_{}.png'.format(title))
         if is_display:
             plt.show()
@@ -68,3 +68,53 @@ def plot_figure5_of_task2(list_of_T, task2_accuracy_of_category, is_display=Fals
         if is_display:
             plt.show()
         plt.close(fig)
+
+
+def plot_multi_figure5_of_task1(list_of_K, task1_accuracy_of_category, is_display=False):
+    fig, axs = plt.subplots(2, 5, figsize=(6.4 * 5, 4.8 * 2))
+    ax_list = [ax for sub_axs in axs for ax in sub_axs]
+
+    for idx, (title, predictions) in enumerate(task1_accuracy_of_category.items()):
+        if len(predictions) == 0:
+            continue
+        ax = ax_list[idx]
+        n_groups = len(list_of_K)
+        index = np.arange(n_groups)
+
+        ax.bar(index, predictions, tick_label=list_of_K, align='center')
+        ax.set_title(title)
+        ax.set_xlim(-1, n_groups)
+        ax.set_ylim(50, 80)
+
+    fig.suptitle('Classification accuracy for each feature class')
+    fig.text(0.5, 0.04, 'Number of observation posts (K)', ha='center')
+    fig.text(0.04, 0.5, 'Accuracy', va='center', rotation='vertical')
+    fig.savefig('output/figure5_multi_task1.png')
+    if is_display:
+        plt.show()
+    plt.close(fig)
+
+
+def plot_multi_figure5_of_task2(list_of_T, task2_accuracy_of_category, is_display=False):
+    fig, axs = plt.subplots(2, 5, figsize=(6.4 * 5, 4.8 * 2))
+    ax_list = [ax for sub_axs in axs for ax in sub_axs]
+
+    for idx, (title, predictions) in enumerate(task2_accuracy_of_category.items()):
+        if len(predictions) == 0:
+            continue
+        ax = ax_list[idx]
+        n_groups = len(list_of_T)
+        index = np.arange(n_groups)
+
+        ax.bar(index, predictions, tick_label=list_of_T, align='center')
+        ax.set_title(title)
+        ax.set_xlim(-1, n_groups)
+        ax.set_ylim(50, 80)
+
+    fig.suptitle('Classification accuracy for each feature class')
+    fig.text(0.5, 0.04, 'Observation period in days (T)', ha='center')
+    fig.text(0.04, 0.5, 'Accuracy', va='center', rotation='vertical')
+    fig.savefig('output/figure5_multi_task2.png')
+    if is_display:
+        plt.show()
+    plt.close(fig)
