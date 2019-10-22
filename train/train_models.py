459426504d4feb2eb080b1dda89ea638b1566881
Jaehwan Park <turnaround0@gmail.com>
2019-10-22 11:45:24 +0900

    Update train_models.py

--- train/train_models.py
+++ train/train_models.py
@@ -9,25 +9,26 @@ warnings.filterwarnings("ignore", category=ConvergenceWarning)
 
 def decision_tree_classifier_with_args(*arg, **kwargs):
     kwargs['random_state'] = 1234
+    # Below parameter is critical for this test
+    kwargs['min_samples_split'] = 0.05
     return DecisionTreeClassifier(*arg, **kwargs)
 
 
 def logistic_regression_with_args(*arg, **kwargs):
-    kwargs['max_iter'] = 1e3
+    kwargs['max_iter'] = 1e4
     kwargs['solver'] = 'lbfgs'
     kwargs['random_state'] = 1234
     return LogisticRegression(*arg, **kwargs)
 
 
 def svc_with_args(*args, **kwargs):
-    kwargs['max_iter'] = 1e3
-    kwargs['gamma'] = 'auto'
+    kwargs['max_iter'] = 1e4
     kwargs['kernel'] = 'rbf'
     kwargs['random_state'] = 1234
     return SVC(*args, **kwargs)
 
 
 def linear_svc_with_args(*args, **kwargs):
-    kwargs['max_iter'] = 1e3
+    kwargs['max_iter'] = 1e4
     kwargs['random_state'] = 1234
     return LinearSVC(*args, **kwargs)
