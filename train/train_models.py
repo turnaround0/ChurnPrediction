625ce20a199d930b0548c7fd9dee28b7792e643f
Jaehwan Park <turnaround0@gmail.com>
2019-10-23 17:12:31 +0900

    Update train_models.py

--- train/train_models.py
+++ train/train_models.py
@@ -39,7 +39,7 @@ def linear_svc_with_args(*args, **kwargs):
 def decision_tree_ext_method(*args, **kwargs):
     kwargs['random_state'] = 1234
     kwargs['min_samples_split'] = 0.05
-    kwargs['min_samples_leaf'] = 100
-    kwargs['max_round'] = 7
+    kwargs['min_samples_leaf'] = 500
+    kwargs['max_round'] = 10
     kwargs['p_value'] = 0.1
     return DecisionTreeExtClassifier(*args, **kwargs)
