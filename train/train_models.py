3727fd84ef5bd7025742acfc12a056670ac0f2b4
Jaehwan Park <turnaround0@gmail.com>
2019-10-23 14:53:37 +0900

    Update train_models.py

--- train/train_models.py
+++ train/train_models.py
@@ -4,6 +4,8 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC, LinearSVC
 from sklearn.exceptions import ConvergenceWarning
 
+from model_dt_ext import DecisionTreeExtClassifier
+
 warnings.filterwarnings("ignore", category=ConvergenceWarning)
 
 
@@ -32,3 +34,11 @@ def linear_svc_with_args(*args, **kwargs):
     kwargs['max_iter'] = 1e4
     kwargs['random_state'] = 1234
     return LinearSVC(*args, **kwargs)
+
+
+def decision_tree_ext_method(**kwargs):
+    kwargs['random_state'] = 1234
+    kwargs['min_samples_split'] = 0.05
+    kwargs['max_round'] = 7
+    kwargs['p_value'] = 0.1
+    return DecisionTreeExtClassifier(**kwargs)
