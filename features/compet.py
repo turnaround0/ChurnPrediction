40a6afeb73633decf82ed3c158e8f0beb8a88dda
Jaehwan Park <turnaround0@gmail.com>
2019-10-24 14:30:44 +0900

    Update compet.py

--- features/compet.py
+++ features/compet.py
@@ -1,5 +1,7 @@
 # Competitiveness features 1: relative_rank_pos
 def getRelRankPos(posts):
+    # average of total # of answers for a question divided by the rank of user's answer
     answers = posts[posts.PostTypeId == 2]
-    answers['ans_score_div_ranks'] = answers.Score / answers.groupby('ParentId').Score.rank(ascending=False)
-    return answers.groupby('OwnerUserId').ans_score_div_ranks.mean()
+    group_score = answers.groupby('ParentId').Score
+    answers['rel_rank_pos'] = group_score .transform('count') / group_score.rank(method='average')
+    return answers.groupby('OwnerUserId').rel_rank_pos.mean()
