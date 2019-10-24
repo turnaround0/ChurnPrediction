e9926f9858c6d272808cc8758031d878e04fe9f7
Jaehwan Park <turnaround0@gmail.com>
2019-10-24 15:43:29 +0900

    Update answering.py

--- features/answering.py
+++ features/answering.py
@@ -1,14 +1,16 @@
-# Gratitude features 1: Average of Answer Count
+# Answering features 1: Average of Answer Count
 def getAvgNumOfAnswerCount(users, answers, questions, qnta, tqna):
-    return answers.groupby('OwnerUserId')['AnswerCount'].mean()
-    
-# Gratitude features 1: First Type of Posting
-def getFirstPostType(posts):
-    tmp = posts[posts['ith'] == 1]['OwnerUserId'].to_frame()
-    tmp = tmp.set_index('OwnerUserId')
-    return posts[posts['OwnerUserId'].isin(tmp.index)]['PostTypeId'].apply(lambda post: 0 if post == 1 else 2)
+    return answers.groupby('OwnerUserId').AnswerCount.mean()
 
-# Gratitude features 1: Total # of comment
+
+# Answering features 1: First Type of Posting
+def getFirstPostType(posts):
+    tmp = posts[posts.ith == 1].OwnerUserId.to_frame()
+    tmp = tmp.set_index('OwnerUserId')
+    return posts[posts.OwnerUserId.isin(tmp.index)].PostTypeId.apply(lambda post_type: 1 if post_type == 2 else 0)
+
+
+# Answering features 1: Total # of comment
 def getTotalNumOfComments(users, answers, questions,  qnta, tqna):
     tqna['total_comment'] = tqna['CommentCountA'] + tqna['CommentCountQ']
-    return tqna.groupby('OwnerUserIdQ')['total_comment'].sum()
+    return tqna.groupby('OwnerUserIdQ').total_comment.sum()
