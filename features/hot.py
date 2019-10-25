54a8c3e2961717ae950164378cddb05293ad784c
Jaehwan Park <turnaround0@gmail.com>
2019-10-25 10:00:36 +0900

    Update hot.py

--- features/hot.py
+++ features/hot.py
@@ -1,11 +1,24 @@
-# Hot features 1: in_hot_topic
-def getNumInHotTopic(posts):
-    questions = posts[posts.PostTypeId == 1][['AnswerCount', 'OwnerUserId']]\
+def prepareTask2(posts):
+    questions = posts[posts.PostTypeId == 1][['AnswerCount', 'OwnerUserId']] \
         .rename(columns={'OwnerUserId': 'QuestionUserId'})
-    answers = posts[posts.PostTypeId == 2][['ParentId', 'OwnerUserId']]\
+    answers = posts[posts.PostTypeId == 2][['ParentId', 'OwnerUserId']] \
         .rename(columns={'OwnerUserId': 'AnswerUserId'})
     hot_answer_count = questions.AnswerCount.quantile(0.95)
 
     hot_questions = questions[questions.AnswerCount >= hot_answer_count]
     hot_qna = hot_questions.merge(answers, left_index=True, right_on='ParentId')
-    return hot_qna.groupby('AnswerUserId').size().add(hot_questions.groupby('QuestionUserId').size(), fill_value=0)
+
+    return hot_questions, hot_qna
+
+
+# Hot features 1: in_hot_topic
+def getNumInHotTopic(questions, qna):
+    return qna.groupby('AnswerUserId').size().add(questions.groupby('QuestionUserId').size(), fill_value=0)
+
+
+def getNumQuestionsInHotTopic(questions):
+    return questions.groupby('QuestionUserId').size()
+
+
+def getNumAnswersInHotTopic(qna):
+    return qna.groupby('AnswerUserId').size()
